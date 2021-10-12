b2b679032d2688fff020d08ba7b44333535a8041
Alejandro Aragón <alejandro@idr-tech.com>
2021-10-12 00:15:41 -0500

    adding plus operator

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import { useState } from "react";
 function App() {
   const [result, setResult] = useState(0);
-  const [operations, setOperations] = useState("");
+  const [operations, setOperations] = useState("5 5 5 8 + + -");
+  const [stack, setStack] = useState([]);
+
+  const handleClick = () => {
+    let array = operations.split(" ");
+
+    for (let i = 0; i < array.length; i++) {
+      let value = array[i];
+      switch (value) {
+        case "+":
+          const newStack = [];
+          newStack.push(...array);
+          console.log(newStack);
+          let value1 = newStack.pop();
+          console.log(value1);
+          let value2 = newStack.pop();
+          console.log(value2);
+
+          setStack([...newStack]);
+
+          setResult(value1 + value2);
+          setStack([...stack, result]);
+          // stack.push(result);
+          break;
+        case "-":
+        case "*":
+        case "/":
+        default:
+          setStack([...stack, parseInt(value)]);
+        // stack.push(parseInt(value));
+      }
+    }
+    setResult(stack.pop());
+  };
 
   return (
     <div>
@@ -11,7 +44,7 @@ function App() {
         value={operations}
         onChange={(e) => setOperations(e.target.value)}
       />
-      <button>Calculate</button>
+      <button onClick={handleClick}>Calculate</button>
       <br />
       <span>{`Result= ${result}`}</span>
       <br />
