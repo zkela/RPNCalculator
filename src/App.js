8b2d325e4e9c25e5a565c1ded95d23c40dc3655d
Alejandro Aragón <alejandro@idr-tech.com>
2021-10-12 09:14:19 -0500

    adding some styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useState } from "react";
 import calculate from "./calculator/calculator";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 function App() {
   const [input, setInput] = useState("");
@@ -19,11 +24,43 @@ function App() {
 
   return (
     <div>
-      <label>Introduce the operations</label>
-      <input type="text" value={input} onChange={(e) => handleInput(e)} />
-      <button onClick={handleClick}>Calculate</button>
+      <Stack spacing={2} direction="row">
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "50ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <div>
+            <TextField
+              label="Introduce Operations"
+              id="outlined-size-small"
+              defaultValue="Small"
+              size="small"
+              value={input}
+              onChange={(e) => handleInput(e)}
+            />
+          </div>
+        </Box>
+        <Button variant="contained" onClick={handleClick}>
+          Calculate
+        </Button>
+      </Stack>
       <br />
-      <span>{`Result= ${result}`}</span>
+      <Typography component="div">
+        <Box
+          sx={{
+            color: "green",
+            fontFamily: "Monospace",
+            fontSize: "h3.fontSize",
+            m: 1,
+          }}
+        >
+          {`Result= ${result}`}
+        </Box>
+      </Typography>
       <br />
       {error !== "" ? <span>{error}</span> : null}
     </div>
